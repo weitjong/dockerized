4434c15d519d7e5836c1e97d051f9fb81f7b8fcf
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2020-07-26 00:46:26 +0800

    Align the copyright starting year with the commit history.

--- urho3d/arm/sysroot/arm_entrypoint.sh
+++ urho3d/arm/sysroot/arm_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2019-2020 Yao Wei Tjong. All rights reserved.
+# Copyright (c) 2018-2020 Yao Wei Tjong. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
